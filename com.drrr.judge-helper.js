584c63f3a76ef12982d61d9147d23558e2300934
Willian Zhang <Willian@Willian-Zhang.com>
2016-06-11 01:33:19 +0800

    feat(judge): set all and default

--- com.drrr.judge-helper.js
+++ com.drrr.judge-helper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         法官 Helper
 // @namespace    com.drrr.judge-helper
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Willian
 // @match        http://drrr.com/room/*
@@ -29,3 +29,40 @@ var resetCounter = function(){
 var changeDisplay = function(sth){
     button.val(sth);
 };
+
+var formDefaultInstruction = function (key, value){
+    return function(){
+        return $.post('',{default:{key:key,value:value}}).done(function(e){swal(e);});
+    };
+};
+var formSetInstruction = function (key, value){
+    var settings = {
+        to:'all'
+    };
+    settings[key] = value;
+    return function(){
+        return $.post('',settings).done(function(e){if(e){swal(e);}});
+    };
+};
+$(unsafeWindow).on('room.user.menu.show',function(event, menu, user, functions){
+    functions.resetDevider();
+    functions.addDevisionIfNot();
+    if(user.hasOwnProperty('player')){
+        var playerTag = t(!user.player ? 'player' : 'non-player');
+        functions.addNode(t('Set all to {1}', playerTag),
+            formSetInstruction('player',!user.player)
+            );
+        functions.addNode(t('Set defalut to {1}', playerTag),
+            formDefaultInstruction('player',!user.player)
+            );
+    }
+    if(user.hasOwnProperty('alive')){
+        var aliveTag = t(!user.alive ? 'alive' : 'dead');
+        functions.addNode(t('Set all to {1}', aliveTag),
+            formSetInstruction('alive',!user.alive)
+            );
+        functions.addNode(t('Set defalut to {1}', aliveTag),
+            formDefaultInstruction('alive',!user.alive)
+            );
+    }
+});
