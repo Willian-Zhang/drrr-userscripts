58801d3a7f00999403b84bc6f3df034a3d9af2ad
Willian Z <willian@willian.email>
2025-01-08 22:23:13 +0800

    judge update state detect

--- com.drrr.judge-helper.js
+++ com.drrr.judge-helper.js
@@ -1,20 +1,24 @@
 // ==UserScript==
-// @name         法官 Helper
+// @name         DRRR 法官 Helper
 // @namespace    com.drrr.judge-helper
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       Willian
 // @match        http://drrr.com/room/*
 // @match        https://drrr.com/room/*
-// @match        http://drrr.local/room*
-// @match        https://drrr.local/room*
+// @match        http://drrr.lan/room*
+// @match        https://drrr.lan/room*
 // @grant        unsafeWindow
 // @downloadURL https://update.greasyfork.org/scripts/20061/%E6%B3%95%E5%AE%98%20Helper.user.js
 // @updateURL https://update.greasyfork.org/scripts/20061/%E6%B3%95%E5%AE%98%20Helper.meta.js
 // ==/UserScript==
 
 // let $ = unsafeWindow.$;
-let window = unsafeWindow;
+
+if(!window){
+    var window = unsafeWindow;
+}
+
 let startRegex = /開始|开始/;
 let button = $('.room-submit-btn');
 $(window).on('room.chat.message', function (_, chat) {
@@ -38,7 +42,7 @@ function changeDisplay(sth) {
 
 function formDefaultInstruction(key, value) {
     return function () {
-        return $.post('', { default: { key: key, value: value } }).done(function (e) { swal(e); });
+        return DRRRAPI.post_legacy({ default: { key: key, value: value } }).catch(function (e) { swal(e); });
     };
 };
 function formSetInstruction(key, value) {
@@ -50,9 +54,9 @@ function formSetInstruction(key, value) {
         return $.post('', settings).done(function (e) { if (e) { swal(e); } });
     };
 };
-const translator = window.translator;
+const trans = window.translator;
 function isEnglish() {
-    return translator.constructor.catalog["Yes"] == "YES"
+    return trans.constructor.catalog["Yes"] == "YES"
 }
 function addTranslation() {
     let catalog = translator.constructor.catalog;
@@ -62,24 +66,35 @@ function addTranslation() {
 if (!isEnglish()) addTranslation();
 
 $(window).on('room.user.menu.show', function (event, menu, user, dropdown) {
-    dropdown.resetDevider();
-    dropdown.addDevisionIfNot();
-    if (user.hasOwnProperty('player')) {
-        let playerTag = t(!user.player ? 'player' : 'non-player');
-        dropdown.addNode(t('Set all to {1}', playerTag),
-            formSetInstruction('player', !user.player)
+    if(DRRRClientBehavior.is_game_room){
+        dropdown.resetDevider();
+        dropdown.addDevisionIfNot();
+        dropdown.addNode(t(`All`), null, 'dropdown-item-unclickable');
+        if (user.hasOwnProperty('player')) {
+            let playerTag = t(!user.player ? 'player' : 'non-player');
+            dropdown.addNode(t('Set all to {1}', playerTag),
+                formSetInstruction('player', !user.player)
+            );
+            
+        }
+        if (user.hasOwnProperty('alive')) {
+            let aliveTag = t(!user.alive ? 'alive' : 'dead');
+            dropdown.addNode(t('Set all to {1}', aliveTag),
+                formSetInstruction('alive', !user.alive)
+            )
+        }
+
+        dropdown.resetDevider();
+        dropdown.addDevisionIfNot();
+        dropdown.addNode(t(`Defaults`), null, 'dropdown-item-unclickable');
+        let is_default_player = DRRRClientBehavior.gameDefaults?.player ?? false
+        dropdown.addNode(t('Set defalut to {1}',  t(!is_default_player ? 'player' : 'non-player')),
+            formDefaultInstruction('player', !is_default_player)
         );
-        dropdown.addNode(t('Set defalut to {1}', playerTag),
-            formDefaultInstruction('player', !user.player)
-        );
-    }
-    if (user.hasOwnProperty('alive')) {
-        let aliveTag = t(!user.alive ? 'alive' : 'dead');
-        dropdown.addNode(t('Set all to {1}', aliveTag),
-            formSetInstruction('alive', !user.alive)
-        );
-        dropdown.addNode(t('Set defalut to {1}', aliveTag),
-            formDefaultInstruction('alive', !user.alive)
-        );
+        let is_default_alive = DRRRClientBehavior.gameDefaults?.alive ?? true
+        dropdown.addNode(t('Set defalut to {1}', t(!is_default_alive ? 'alive' : 'dead')),
+            formDefaultInstruction('alive', !is_default_alive)
+        );    
+        dropdown.resetDevider();
     }
 });
